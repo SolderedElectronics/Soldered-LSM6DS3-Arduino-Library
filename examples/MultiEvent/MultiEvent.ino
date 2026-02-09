21b7abe3e18179928b66306d53cb1c24ea92c4ec
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2026-02-09 09:35:26 +0100

    Update MultiEvent.ino

--- examples/MultiEvent/MultiEvent.ino
+++ examples/MultiEvent/MultiEvent.ino
@@ -1,55 +1,25 @@
 /**
- ******************************************************************************
- * @file    MultiEvent.ino
- * @author  AST
- * @version V1.0.0
- * @date    7 September 2017
- * @brief   Arduino test application for the STMicrolectronics X-NUCLEO-IKS01A1
- *          MEMS Inertial and Environmental sensor expansion board.
- *          This application detects free fall, tap, double tap, tilt, wake up,
- *          6D Orientation and step events through the LSM6DS3 sensor.
- *          This application makes use of C++ classes obtained from the C
- *          components' drivers.
- ******************************************************************************
- * @attention
+ *******************************************************************************************
  *
- * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+ * @file      Pedometer.ino
+ * @brief     This example will show you how to use the pedometer feature.
+ *            Connect the IMU to the board via the Qwiic connector and the interrupt pin to the digital pin 5.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ * @link      solde.red/333069
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************
- */
+ * @author    Josip Šimun Kuči @ Soldered
+ *******************************************************************************************/
 
-/** NOTE
-  In order to use this sketch on X-NUCLEO-IKS01A1 board you need the STEVAL-MKI160V1
-  board attached to X-NUCLEO-IKS01A1 board via DIL24 interface. 
-*/
 
 // Includes.
 #include <LSM6DS3-SOLDERED.h>
 
 // Components.
 Soldered_LSM6DS3 lsm6ds3;
+
+// Interrupt pin.
+#define INT1_PIN 5
+#define INT2_PIN 4
 
 //Interrupts.
 volatile int mems_event = 0;
@@ -63,17 +33,14 @@ void sendOrientation();
 
 void setup() {
   // Led.
-  pinMode(13, OUTPUT);
-
-  // Initialize serial for output.
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   // Initialize I2C bus.
   Wire.begin();
 
   //Interrupts.
-  attachInterrupt(A2, INT1Event_cb, RISING);
-  attachInterrupt(A3, INT2Event_cb, RISING);
+  attachInterrupt(INT1_PIN, INT1Event_cb, RISING);
+  attachInterrupt(INT2_PIN, INT2Event_cb, RISING);
 
   // Initialize Components.
   lsm6ds3.begin();
